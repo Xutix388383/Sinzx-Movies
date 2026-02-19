2b8ce5076b853501aa161ccd69c60accb8c125d7
Xutix388383 <moddedpgd31@gmail.com>
2026-02-18 19:08:37 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const CONFIG = {
     // TMDB API KEY - User will need to replace this if the demo key hits limits
     // Using a temporary public demo key pattern or instructing user to replace it.
     // For now I will leave a placeholder that clearly indicates where to put it.
-    TMDB_API_KEY: 'YOUR_TMDB_API_KEY_HERE', 
+    TMDB_API_KEY: '9f3a94034c696a33133c40385d030817',
     TMDB_BASE_URL: 'https://api.themoviedb.org/3',
     IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/original',
     POSTER_BASE_URL: 'https://image.tmdb.org/t/p/w500'
